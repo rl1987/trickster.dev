0e038896def518eca985871b9bbe7f0e80d7155d
rl1987 <rl1987@users.noreply.github.com>
2021-12-11 19:56:01 +0200

    Small tweaks to run_onion_service.py

--- run_onion_service.py
+++ run_onion_service.py
@@ -5,14 +5,17 @@ from stem.control import Controller
 import http.server
 import os
 import subprocess
+import sys
 
 def main():
     controller = Controller.from_port()
     controller.authenticate()
     response = controller.create_ephemeral_hidden_service({80: 8080}, await_publication=True)
 
+    assert(len(response.service_id) > 0)
+
     onion_url = "http://" + response.service_id + ".onion"
-    print(onion_url)
+    print(onion_url, file=sys.stderr)
 
     os.chdir("tricksterblog/")
 
