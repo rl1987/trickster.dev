a8546c679c770903a87695ba0569503543e5c465
rl1987 <rl1987@users.noreply.github.com>
2022-06-11 10:14:45 +0300

    Run black to fix formatting in run_onion_service.py

--- run_onion_service.py
+++ run_onion_service.py
@@ -7,12 +7,15 @@ import os
 import subprocess
 import sys
 
+
 def main():
     controller = Controller.from_port()
     controller.authenticate()
-    response = controller.create_ephemeral_hidden_service({80: 8080}, await_publication=True)
+    response = controller.create_ephemeral_hidden_service(
+        {80: 8080}, await_publication=True
+    )
 
-    assert(len(response.service_id) > 0)
+    assert len(response.service_id) > 0
 
     onion_url = "http://" + response.service_id + ".onion"
     print(onion_url, file=sys.stderr)
@@ -28,4 +31,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
